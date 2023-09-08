52dab86111f2d4e2ebf1d3a6a9f00bbbb6f933f6
fatom <fatom2k10@gmail.com>
2023-09-08 18:36:25 +0200

    update main.pp Works correctly with canbus

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,91 @@
 #include <Arduino.h>
-#include <ESP32CAN.h>
-#include <CAN_config.h>
+#include <driver/can.h>
+#include <driver/gpio.h>
 
-CAN_device_t CAN_cfg;               // CAN Config
-unsigned long previousMillis = 0;   // will store last time a CAN Message was send
-const int interval = 1000;          // interval at which send CAN Messages (milliseconds)
-const int rx_queue_size = 10;       // Receive Queue size
+byte gaz=0x00;
+
+void setup_can_driver() {
+  can_general_config_t general_config {
+    .mode = CAN_MODE_NORMAL,
+    .tx_io = (gpio_num_t)GPIO_NUM_22,
+    .rx_io = (gpio_num_t)GPIO_NUM_21,
+    .clkout_io = (gpio_num_t)CAN_IO_UNUSED,
+    .bus_off_io = (gpio_num_t)CAN_IO_UNUSED,
+    .tx_queue_len = 0,
+    .rx_queue_len = 65,
+    .alerts_enabled = CAN_ALERT_ALL,
+    .clkout_divider = 0
+  };
+
+  can_timing_config_t timing_config = CAN_TIMING_CONFIG_125KBITS();
+  can_filter_config_t filter_config = CAN_FILTER_CONFIG_ACCEPT_ALL();
+
+  esp_err_t error;
+  error = can_driver_install(&general_config,&timing_config,&filter_config);
+
+  if(error==ESP_OK) {
+    Serial.println("Can driver installation OK");
+  } else {
+    Serial.println("Can driver installation NOK");
+    return;
+  };
+
+  error = can_start();
+
+  if(error==ESP_OK){
+    Serial.println("Can Driver started");
+  }else{
+    Serial.println("Can Driver failed to start");
+    return;
+  }
+
+}
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Basic Demo - ESP32-Arduino-CAN");
-  CAN_cfg.speed = CAN_SPEED_125KBPS;
-  CAN_cfg.tx_pin_id = GPIO_NUM_22;
-  CAN_cfg.rx_pin_id = GPIO_NUM_21;
-  CAN_cfg.rx_queue = xQueueCreate(rx_queue_size, sizeof(CAN_frame_t));
-  // Init CAN Module
-  ESP32Can.CANInit();
-  sleep(1000);
+  Serial.begin(115200);
+  
+  setup_can_driver();
+
+  sleep(2);
   Serial.println(" > Setup Complete");
 }
 
+
+
 void loop() {
 
-  CAN_frame_t rx_frame;
-
-  unsigned long currentMillis = millis();
-
-  // Receive next CAN frame from queue
-  if (xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE) {
-
-    if (rx_frame.FIR.B.FF == CAN_frame_std) {
-      printf("New standard frame");
+  can_message_t rx_frame;
+  if(can_receive(&rx_frame,pdMS_TO_TICKS(1000))==ESP_OK){
+    /**
+    printf("from 0x%08X,DLC%d,Data",rx_frame.identifier,rx_frame.data_length_code);
+    for(int i=0;i<rx_frame.data_length_code;i++){
+      printf(" 0x%02X ", rx_frame.data[i]);
     }
-    else {
-      printf("New extended frame");
-    }
+    printf("\n");
+    */
 
-    if (rx_frame.FIR.B.RTR == CAN_RTR) {
-      printf(" RTR from 0x%08X, DLC %d\r\n", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
-    }
-    else {
-      printf(" from 0x%08X, DLC %d, Data ", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
-      for (int i = 0; i < rx_frame.FIR.B.DLC; i++) {
-        printf("0x%02X ", rx_frame.data.u8[i]);
+    if (rx_frame.identifier==0x00000080){
+
+
+// 122 max
+// 0x12 > 18
+
+      if(gaz!=rx_frame.data[4]){
+        //printf("Gaz: 0x%02X", rx_frame.data[4]);
+        int readvalue = rx_frame.data[4];
+        int outvalue = (readvalue*100)/122;
+        Serial.print("Gaz: ");
+        Serial.print(outvalue);
+        Serial.println("%");
+        gaz=rx_frame.data[4];
       }
-      printf("\n");
-    }
-  }
 
+
+
+    }
+
+    
+
+  }
+  
 }
